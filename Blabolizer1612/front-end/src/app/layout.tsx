14c9ac07625857a09fe317646c2f8a9b8d0ed2ca
ffups <142579057+ffups@users.noreply.github.com>
2025-04-18 18:07:36 +0200

    feat: replace next/head with next/script for Matomo tracking code integration

--- Blabolizer1612/front-end/src/app/layout.tsx
+++ Blabolizer1612/front-end/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Head from "next/head"; // Add this import
+import Script from "next/script"; // Use next/script instead of next/head
 
 export const metadata: Metadata = {
   title: "Blabolizer1612",
@@ -14,26 +14,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        {/* Matomo Tracking Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var _paq = window._paq = window._paq || [];
-              _paq.push(['trackPageView']);
-              _paq.push(['enableLinkTracking']);
-              (function() {
-                var u="//34.51.152.111/matomo/";
-                _paq.push(['setTrackerUrl', u+'matomo.php']);
-                _paq.push(['setSiteId', '1']);
-                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-              })();
-            `,
-          }}
-        />
-      </Head>
+     
       <body >
+         {/* Matomo Tracking Code */}
+         <Script id="matomo" strategy="afterInteractive">
+          {`
+            var _paq = window._paq = window._paq || [];
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//34.51.152.111/matomo/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `}
+        </Script>
         {children}
       </body>
     </html>
