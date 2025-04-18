bbccae10bfd106b99dfc7694255a5c4a43974b23
ffups <142579057+ffups@users.noreply.github.com>
2025-04-18 19:39:41 +0200

    fixed tracking link i think?

--- Blabolizer1612/front-end/src/app/layout.tsx
+++ Blabolizer1612/front-end/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
             _paq.push(['trackPageView']);
             _paq.push(['enableLinkTracking']);
             (function() {
-             var u="https://matomo.matomotes.me/matomo/";
+             var u="https://matomo.matomotes.me/";
               _paq.push(['setTrackerUrl', u+'matomo.php']);
               _paq.push(['setSiteId', '1']);
               var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
